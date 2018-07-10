b65972f7a28a76b1b43ba79f332514c09d304b49
Dina Hafez <dinahafez.8@gmail.com>
2018-07-10 23:17:37 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,7 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"].each do |attribute, value|
-    attribute.delete_if {|value| value == "strawberry"}
+  contacts["Freddy Mercury"].delete_if 
   end
      
   #remember to return your newly altered contacts hash!
